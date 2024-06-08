ed0b674c1b4d6c7c5cea659d2f01eef0e1ca4ba8
craftbox <bartoszdudzinski18@gmail.com>
2024-06-08 02:41:44 +0200

    pretty much all logic

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,334 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode"
+import {
+	deleteScope,
+	getScopeConfig,
+	hideFile,
+	loadFromSettings,
+	renameScope,
+	revealFiles,
+	saveToSettings,
+	updateFileExcluding,
+} from "./scope"
+import {
+	createScope,
+	getScopes,
+	parseScopeUri,
+	scopeExist,
+	scopeFolder,
+	selectScope,
+	setScopeConfig,
+} from "./scope"
+
+export const RootScope = "Root"
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	context.workspaceState.update("scopes", undefined)
+	context.workspaceState.update("config", undefined)
+	loadFromSettings(context)
+	// setup
+	setScopeConfig(context, {
+		currentScope: RootScope,
+		...getScopeConfig(context),
+	})
+	if (!scopeExist(context, { name: RootScope }))
+		createScope(context, { name: RootScope })
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "folder-scopes" is now active!');
+	const scopeLabel = vscode.window.createStatusBarItem(
+		vscode.StatusBarAlignment.Left,
+		1
+	)
+	scopeLabel.command = "folder-scopes.selectScope"
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('folder-scopes.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Folder Scopes!');
-	});
+	context.subscriptions.push(scopeLabel)
 
-	context.subscriptions.push(disposable);
+	scopeLabel.text = getScopeConfig(context)?.currentScope ?? RootScope
+	scopeLabel.tooltip = "Current Scope"
+	scopeLabel.name = "Current Scope"
+	scopeLabel.show()
+
+	const workspaceFolderPaths = vscode.workspace.workspaceFolders?.map(
+		(v) => v.uri.path
+	)
+
+	const vscodeFolderPaths = vscode.workspace.workspaceFolders?.map(
+		(v) => v.uri.path + "/.vscode"
+	)
+
+	const excludedPaths = [
+		...(workspaceFolderPaths ?? []),
+		...(vscodeFolderPaths ?? []),
+	]
+
+	vscode.commands.executeCommand("setContext", "excludedPaths", excludedPaths)
+
+	vscode.workspace.onDidChangeConfiguration((e) => {
+		const affected = e.affectsConfiguration("folder-scopes.scopes")
+		if (affected) {
+			loadFromSettings(context)
+			updateFileExcluding(context)
+		}
+	})
+
+	const disposableSelectScope = vscode.commands.registerCommand(
+		"folder-scopes.selectScope",
+		(...commandArgs) => {
+			const scopes = getScopes(context)
+			vscode.window
+				.showQuickPick(scopes, {
+					title: "Select Scope",
+					placeHolder: "Select a Scope to Switch to",
+				})
+				.then((v) => {
+					if (v) {
+						const prevScope = getScopeConfig(context)?.currentScope
+						scopeLabel.text = v
+						selectScope(context, v)
+						if (v === prevScope) {
+							vscode.window.showInformationMessage(
+								`Reloaded "${v}" scope`
+							)
+						} else {
+							vscode.window.showInformationMessage(
+								`Switched to "${v}" scope`
+							)
+						}
+					}
+				})
+		}
+	)
+
+	const disposableScopeFolder = vscode.commands.registerCommand(
+		"folder-scopes.scopeFolder",
+		(...commandArgs) => {
+			if (getScopeConfig(context)?.currentScope === RootScope) {
+				vscode.window.showErrorMessage(
+					`Can't scope folder in ${RootScope} scope. Create a new scope to scope a folder.`
+				)
+				return
+			}
+
+			const scope = parseScopeUri(commandArgs[0] as vscode.Uri)
+			scopeFolder(context, scope)
+			saveToSettings(context)
+		}
+	)
+
+	const disposableCreateScope = vscode.commands.registerCommand(
+		"folder-scopes.createScope",
+		(...commandArgs) => {
+			vscode.window
+				.showInputBox({
+					title: "Scope Name",
+					placeHolder: "Enter Name for the Scope",
+					validateInput(value) {
+						if (
+							scopeExist(context, {
+								name: value,
+							})
+						) {
+							return "Scope with this name already exists"
+						}
+
+						return null
+					},
+				})
+				.then((v) => {
+					if (v) {
+						createScope(context, { name: v })
+						vscode.window.showInformationMessage(
+							`Scope "${v}" has been created`
+						)
+						scopeLabel.text = v
+						selectScope(context, v)
+						vscode.window.showInformationMessage(
+							`Switched to "${v}" scope`
+						)
+						saveToSettings(context)
+					}
+				})
+		}
+	)
+
+	const disposableDeleteScope = vscode.commands.registerCommand(
+		"folder-scopes.deleteScope",
+		(...commandArgs) => {
+			const scopes = getScopes(context).filter((v) => v !== RootScope)
+			vscode.window
+				.showQuickPick(scopes, {
+					title: "Delete Scope",
+					placeHolder: "Select a Scope to Delete",
+				})
+				.then((v) => {
+					if (v) {
+						if (getScopeConfig(context)?.currentScope === v) {
+							scopeLabel.text = RootScope
+							selectScope(context, RootScope)
+						}
+
+						deleteScope(context, v)
+						vscode.window.showInformationMessage(
+							`Scope "${v}" has been deleted`
+						)
+						saveToSettings(context)
+					}
+				})
+		}
+	)
+
+	const disposableHideFile = vscode.commands.registerCommand(
+		"folder-scopes.hideFile",
+		(...commandArgs) => {
+			if (getScopeConfig(context)?.currentScope === RootScope) {
+				vscode.window.showErrorMessage(
+					`Can't hide a file in ${RootScope} scope. Create a new scope to hide a file`
+				)
+				return
+			}
+
+			const scope = parseScopeUri(commandArgs[0] as vscode.Uri)
+			hideFile(context, {
+				workspaceFolder: scope.workspaceFolder,
+				filePath: scope.folderPath,
+			})
+			saveToSettings(context)
+		}
+	)
+
+	const disposableHideFolder = vscode.commands.registerCommand(
+		"folder-scopes.hideFolder",
+		(...commandArgs) => {
+			if (getScopeConfig(context)?.currentScope === RootScope) {
+				vscode.window.showErrorMessage(
+					`Can't hide a folder in ${RootScope} scope. Create a new scope to hide a folder`
+				)
+				return
+			}
+
+			const scope = parseScopeUri(commandArgs[0] as vscode.Uri)
+			hideFile(context, {
+				workspaceFolder: scope.workspaceFolder,
+				filePath: scope.folderPath,
+			})
+			saveToSettings(context)
+		}
+	)
+
+	const disposableUnscope = vscode.commands.registerCommand(
+		"folder-scopes.unscope",
+		(...commandArgs) => {
+			if (getScopeConfig(context)?.currentScope === RootScope) {
+				return
+			}
+
+			vscode.workspace.workspaceFolders?.forEach((workspaceFolder) => {
+				scopeFolder(context, {
+					folderPath: "",
+					workspaceFolder: workspaceFolder.name,
+				})
+			})
+			saveToSettings(context)
+		}
+	)
+
+	const disposableRevealFiles = vscode.commands.registerCommand(
+		"folder-scopes.revealFiles",
+		(...commandArgs) => {
+			if (getScopeConfig(context)?.currentScope === RootScope) {
+				return
+			}
+
+			vscode.workspace.workspaceFolders?.forEach((workspaceFolder) => {
+				revealFiles(context, {
+					workspaceFolder: workspaceFolder.name,
+				})
+			})
+			saveToSettings(context)
+		}
+	)
+
+	const disposableRenameScope = vscode.commands.registerCommand(
+		"folder-scopes.renameScope",
+		(...commandArgs) => {
+			const scopes = getScopes(context)
+			vscode.window
+				.showQuickPick(scopes, {
+					title: "Select Scope",
+					placeHolder: "Select a Scope to Rename",
+				})
+				.then((scopeName) => {
+					if (scopeName) {
+						vscode.window
+							.showInputBox({
+								title: "Scope Name",
+								placeHolder: "Enter Name for the Scope",
+								validateInput(value) {
+									if (scopeName === value) {
+										return "This is the same name"
+									}
+
+									if (
+										scopeExist(context, {
+											name: value,
+										})
+									) {
+										return "Scope with this name already exists"
+									}
+
+									return null
+								},
+							})
+							.then((newScopeName) => {
+								if (newScopeName) {
+									// createScope(context, { name: newScopeName })
+									if (
+										getScopeConfig(context)
+											?.currentScope === scopeName
+									) {
+										scopeLabel.text = newScopeName
+									}
+
+									renameScope(
+										context,
+										scopeName,
+										newScopeName
+									)
+
+									vscode.window.showInformationMessage(
+										`Scope "${scopeName}" has been renamed to "${newScopeName}"`
+									)
+									saveToSettings(context)
+								}
+							})
+					}
+				})
+		}
+	)
+
+	context.subscriptions.push(
+		disposableSelectScope,
+		disposableScopeFolder,
+		disposableCreateScope,
+		disposableDeleteScope,
+		disposableHideFile,
+		disposableHideFolder,
+		disposableUnscope,
+		disposableRevealFiles,
+		disposableRenameScope
+	)
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() {
+	vscode.workspace.workspaceFolders?.forEach((workspaceFolder) => {
+		const workspaceFolderFiles = vscode.workspace.getConfiguration(
+			"files",
+			workspaceFolder
+		)
+
+		workspaceFolderFiles.update("exclude", {})
+	})
+}
---
+++ src/scope.ts
@@ -0,0 +1,454 @@
+import * as vscode from "vscode"
+import { RootScope } from "./extension"
+import path from "path"
+
+interface FileScopesWorkspaceState {
+	user: {
+		currentScope: string
+	}
+	scopes: {
+		[scopeName: string]: {
+			[workspaceFolder: string]: {
+				folderPath?: string
+				excludedFilePaths?: string[]
+			}
+		}
+	}
+}
+
+export function getWorkspaceFolderScope(
+	context: vscode.ExtensionContext,
+	scope: { workspaceFolder: string; name: string }
+) {
+	const scopeWorkspaceFolders = getScope(context, scope)
+	if (scopeWorkspaceFolders)
+		return scopeWorkspaceFolders[scope.workspaceFolder]
+}
+
+export function getScope(
+	context: vscode.ExtensionContext,
+	scope: { name: string }
+) {
+	const workspaceState = context.workspaceState
+	const targetScope =
+		workspaceState.get<FileScopesWorkspaceState["scopes"]>("scopes")?.[
+			scope.name
+		]
+
+	return targetScope
+}
+
+function setWorkspaceFolderScope(
+	context: vscode.ExtensionContext,
+	scope: {
+		workspaceFolder: string
+		name: string
+		folderPath: string
+		excludedFilePaths?: string[]
+	}
+) {
+	const workspaceState = context.workspaceState
+
+	const currentScope = getScope(context, scope)
+
+	let excludedFilePaths
+	if (
+		currentScope &&
+		Object.keys(currentScope).includes(scope.workspaceFolder)
+	) {
+		excludedFilePaths =
+			currentScope[scope.workspaceFolder].excludedFilePaths
+	}
+
+	if (scope.excludedFilePaths) {
+		excludedFilePaths = scope.excludedFilePaths
+	}
+
+	const updatedWorkspaceScopes: FileScopesWorkspaceState["scopes"][string] = {
+		...currentScope,
+		[scope.workspaceFolder]: {
+			folderPath: scope.folderPath,
+			excludedFilePaths: excludedFilePaths,
+		},
+	}
+
+	const updatedScopes: FileScopesWorkspaceState["scopes"] = {
+		...workspaceState.get("scopes"),
+		[scope.name]: updatedWorkspaceScopes,
+	}
+
+	// const updatedScopes: FileScopesWorkspaceState["scopes"][string] = {
+	// 	...(getScope(context, scope) ?? {}),
+	// 	[scope.name]: {
+	// 		folderPath: scope.folderPath,
+	// 		excludedFilePaths: scope.excludedFilePaths,
+	// 	},
+	// }
+
+	workspaceState.update("scopes", updatedScopes)
+}
+
+function setScope(
+	context: vscode.ExtensionContext,
+	scope: {
+		name: string
+	}
+) {
+	const workspaceState = context.workspaceState
+	const scopes =
+		workspaceState.get<FileScopesWorkspaceState["scopes"]>("scopes")
+
+	workspaceState.update("scopes", {
+		...scopes,
+		[scope.name]: {},
+	})
+}
+
+export function scopeFolder(
+	context: vscode.ExtensionContext,
+	scope: {
+		workspaceFolder: string
+		folderPath: string
+	}
+) {
+	setWorkspaceFolderScope(context, {
+		...scope,
+		name: getScopeConfig(context)!.currentScope,
+	})
+	updateFileExcluding(context)
+}
+
+export function hideFile(
+	context: vscode.ExtensionContext,
+	scope: {
+		workspaceFolder: string
+		filePath: string
+	}
+) {
+	const curretWorkspaceFolderScope = getWorkspaceFolderScope(context, {
+		workspaceFolder: scope.workspaceFolder,
+		name: getScopeConfig(context)!.currentScope,
+	})!
+
+	setWorkspaceFolderScope(context, {
+		workspaceFolder: scope.workspaceFolder,
+		name: getScopeConfig(context)!.currentScope,
+		folderPath: "",
+		...curretWorkspaceFolderScope,
+		excludedFilePaths: [
+			...(curretWorkspaceFolderScope.excludedFilePaths ?? []),
+			scope.filePath,
+		],
+		// name: getScopeConfig(context)!.currentScope,
+	})
+	updateFileExcluding(context)
+}
+
+export function revealFiles(
+	context: vscode.ExtensionContext,
+	scope: {
+		workspaceFolder: string
+	}
+) {
+	const curretWorkspaceFolderScope = getWorkspaceFolderScope(context, {
+		workspaceFolder: scope.workspaceFolder,
+		name: getScopeConfig(context)!.currentScope,
+	})!
+
+	setWorkspaceFolderScope(context, {
+		workspaceFolder: scope.workspaceFolder,
+		name: getScopeConfig(context)!.currentScope,
+		folderPath: "",
+		...curretWorkspaceFolderScope,
+		excludedFilePaths: [],
+	})
+	updateFileExcluding(context)
+}
+
+export function createScope(
+	context: vscode.ExtensionContext,
+	scope: { name: string }
+) {
+	if (scopeExist(context, scope)) {
+		vscode.window.showErrorMessage(
+			`Scope with name "${scope.name}" already exists`
+		)
+		return
+	}
+
+	setScope(context, scope)
+}
+
+export function scopeExist(
+	context: vscode.ExtensionContext,
+	scope: { name: string }
+) {
+	return typeof getScope(context, scope) !== "undefined"
+}
+
+export function parseScopeUri(folderUri: vscode.Uri) {
+	const folderUriWorkspace = vscode.workspace.getWorkspaceFolder(folderUri)
+	const folderUriRelative = vscode.workspace.asRelativePath(folderUri)
+
+	return {
+		folderPath: folderUriRelative,
+		workspaceFolder: folderUriWorkspace?.name ?? "",
+	}
+}
+
+/**
+ *
+ * @returns array of names of scopes
+ */
+export function getScopes(context: vscode.ExtensionContext) {
+	const workspaceState = context.workspaceState
+	const scopes =
+		workspaceState.get<FileScopesWorkspaceState["scopes"]>("scopes")
+
+	if (scopes) return Object.keys(scopes)
+
+	return []
+}
+
+export function getScopeConfig(context: vscode.ExtensionContext) {
+	const workspaceState = context.workspaceState
+	const config = workspaceState.get<FileScopesWorkspaceState["user"]>("user")
+
+	return config
+}
+
+export function setScopeConfig(
+	context: vscode.ExtensionContext,
+	config: Partial<FileScopesWorkspaceState["user"]>
+) {
+	const workspaceState = context.workspaceState
+	const savedConfig: Partial<FileScopesWorkspaceState["user"]> =
+		workspaceState.get<FileScopesWorkspaceState["user"]>("user") ?? {}
+
+	const newConfig = {
+		...savedConfig,
+		...config,
+	}
+
+	workspaceState.update("user", newConfig)
+}
+
+export function selectScope(
+	context: vscode.ExtensionContext,
+	scopeName: string
+) {
+	setScopeConfig(context, {
+		currentScope: scopeName,
+	})
+	// removeFileExcluding(context)
+	updateFileExcluding(context)
+}
+
+function generatePattern(fragment: string) {
+	const splitFragment = fragment.split("")
+	const patternFragments = []
+
+	splitFragment.forEach((v, i, arr) => {
+		if (i == splitFragment.length - 1) {
+			const prefix = arr.slice(0, i).join("")
+			patternFragments.push(`${prefix}[!${v}]*`)
+			return
+		}
+
+		const prefix = arr.slice(0, i).join("")
+		patternFragments.push(prefix + v)
+		patternFragments.push(`${prefix}[!${v}]*`)
+	})
+
+	patternFragments.push(`${fragment}?*`)
+
+	return `{${patternFragments.join()}}`
+}
+
+function generateExcludePaths(
+	context: vscode.ExtensionContext,
+	scope: {
+		workspaceFolder: string
+		name: string
+	}
+) {
+	const workspaceFolderScope = getWorkspaceFolderScope(context, scope)
+
+	if (!workspaceFolderScope) {
+		return []
+	}
+
+	const folderPath = workspaceFolderScope?.folderPath?.replace(/^\//, "")
+
+	const pathFragments = folderPath?.split("/") ?? []
+
+	const filePaths = workspaceFolderScope.excludedFilePaths?.map((v) =>
+		v.replace(/^\//, "")
+	)
+	const excludePaths: string[] = [...(filePaths ?? [])]
+
+	pathFragments.forEach((v, i, arr) => {
+		if (i == 0 && v === "") return
+
+		if (i == 0) {
+			const pattern = generatePattern(v)
+			excludePaths.push(pattern)
+			// excludePaths.push(`${v}?*`)
+			// excludePaths.push(`.[!${v}]*`)
+			return
+		}
+
+		function genPath(index = i): string {
+			if (index - 1 < 0) return ""
+
+			return `${genPath(index - 1)}${arr[index - 1]}/`
+		}
+
+		const backPath = genPath()
+
+		const pattern = generatePattern(v)
+		excludePaths.push(`${backPath}${pattern}`)
+		// excludePaths.push(`${backPath}${v}?*`)
+		// excludePaths.push(`.${backPath}*`)
+	})
+
+	return excludePaths
+}
+
+function removeFileExcluding(context: vscode.ExtensionContext) {
+	const workspaceFolders = vscode.workspace.workspaceFolders
+
+	if (workspaceFolders) {
+		workspaceFolders.forEach((v) => {
+			const pathsToExclude = generateExcludePaths(context, {
+				workspaceFolder: v.name,
+				name: getScopeConfig(context)?.currentScope ?? "",
+			})
+
+			const workspaceFolderFiles = vscode.workspace.getConfiguration(
+				"files",
+				v
+			)
+
+			const currentExclude = workspaceFolderFiles.get(
+				"exclude"
+			) as Record<string, true>
+
+			const filteredExclude = Object.entries(currentExclude).filter((v) =>
+				pathsToExclude.some((vI) => vI == v[0])
+			)
+
+			workspaceFolderFiles.update(
+				"exclude",
+				Object.fromEntries(filteredExclude)
+			)
+
+			workspaceFolderFiles
+		})
+	}
+}
+
+export function updateFileExcluding(context: vscode.ExtensionContext) {
+	const workspaceFolders = vscode.workspace.workspaceFolders
+
+	if (workspaceFolders) {
+		workspaceFolders.forEach((workspaceFolder) => {
+			const pathsToExclude = generateExcludePaths(context, {
+				workspaceFolder: workspaceFolder.name,
+				name: getScopeConfig(context)?.currentScope!,
+			})
+
+			const pathsToExcludeObj = Object.fromEntries(
+				pathsToExclude?.map((v) => [v, true])
+			)
+
+			const workspaceFolderFiles = vscode.workspace.getConfiguration(
+				"files",
+				workspaceFolder
+			)
+
+			// const currentExclude = workspaceFolderFiles.get(
+			// 	"exclude"
+			// ) as Record<string, true>
+
+			workspaceFolderFiles.update("exclude", {
+				...pathsToExcludeObj,
+				// ...currentExclude,
+			})
+
+			// const revealFolderPath = getWorkspaceFolderScope(context, {
+			// 	workspaceFolder: workspaceFolder.name,
+			// 	name: getScopeConfig(context)?.currentScope!,
+			// })?.folderPath!
+
+			// const revealFolderUri = workspaceFolder.uri.with({
+			// 	path: revealFolderPath,
+			// })
+
+			// const a = vscode.workspace.asRelativePath("**/" + revealFolderPath)
+		})
+	}
+}
+
+export function deleteScope(
+	context: vscode.ExtensionContext,
+	scopeName: string
+) {
+	if (scopeName === RootScope) return
+
+	const workspaceState = context.workspaceState
+	const scopes =
+		workspaceState.get<FileScopesWorkspaceState["scopes"]>("scopes")!
+
+	const filteredScopes = Object.entries(scopes).filter(
+		(v) => v[0] !== scopeName
+	)
+
+	const filteredScopesObject = Object.fromEntries(filteredScopes)
+
+	workspaceState.update("scopes", filteredScopesObject)
+}
+
+export function renameScope(
+	context: vscode.ExtensionContext,
+	scopeName: string,
+	newScopeName: string
+) {
+	if (scopeName === RootScope) return
+
+	const workspaceState = context.workspaceState
+	const scopes =
+		workspaceState.get<FileScopesWorkspaceState["scopes"]>("scopes")!
+
+	const renamedScopes = Object.entries(scopes).map((v) => {
+		if (v[0] === scopeName) {
+			return [newScopeName, v[1]]
+		}
+
+		return v
+	})
+
+	const renamedScopesObject = Object.fromEntries(renamedScopes)
+
+	workspaceState.update("scopes", renamedScopesObject)
+	selectScope(context, newScopeName)
+}
+
+export function saveToSettings(context: vscode.ExtensionContext) {
+	const workspaceState = context.workspaceState
+	const scopes =
+		workspaceState.get<FileScopesWorkspaceState["scopes"]>("scopes")!
+
+	const extensionSettings = vscode.workspace.getConfiguration("folder-scopes")
+
+	extensionSettings.update("scopes", scopes)
+}
+
+export function loadFromSettings(context: vscode.ExtensionContext) {
+	const workspaceState = context.workspaceState
+
+	const extensionSettings = vscode.workspace.getConfiguration("folder-scopes")
+
+	const scopesFromSettings = extensionSettings.get("scopes")
+	workspaceState.update("scopes", scopesFromSettings)
+}
